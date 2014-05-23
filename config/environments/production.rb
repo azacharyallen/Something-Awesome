ded675be8102e2b9f2d047deb9658641bee7b43b
A. Zachary Allen <azacharyallen@gmail.com>
2014-05-22 19:10:05 -0700

    Fixes issue with Boostrap assets in production

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,6 +1,9 @@
 Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
 
+  #precompile filetypes needed by bootstrap
+  config.assets.precompile += %w( '.woff', '.eot', '.svg', '.ttf' )
+
   # Code is not reloaded between requests.
   config.cache_classes = true
 
@@ -27,7 +30,7 @@ Rails.application.configure do
   # config.assets.css_compressor = :sass
 
   # Do not fallback to assets pipeline if a precompiled asset is missed.
-  config.assets.compile = false
+  config.assets.compile = true
 
   # Generate digests for assets URLs.
   config.assets.digest = true
