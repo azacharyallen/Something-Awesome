15c81433fc87133f7d2edb00262142b443cdd54c
A. Zachary Allen <azacharyallen@gmail.com>
2014-05-28 15:23:02 -0700

    Dynamic Page Titles

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,6 @@
 module ApplicationHelper
+  def full_title(page_title)
+    base_title = "The Something Awesome Forums"
+    page_title.empty? ?  base_title : "#{page_title} - #{base_title}"
+ end
 end
